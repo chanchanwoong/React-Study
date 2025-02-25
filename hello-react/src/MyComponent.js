d1646f03cdb4bb69cc3b61d207516f4a63a639c2
Chanwoong <cksdnddlek99@gmail.com>
2025-02-25 17:55:57 +0900

    add: 구조분해 할당으로 props 처리 #02

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,12 +1,6 @@
 import React from "react";
 
-const MyComponent = (props) => {
-  /*
-    구조분해 할당으로 props 처리
-    - 기존에는 props.name, props.children 식으로 처리했으나, 
-      구조분해 할당으로 간단하게 props 처리 가능
-  */
-  const { name, children } = props;
+const MyComponent = ({ name, children }) => {
   return (
     <div>
       안녕하세요. 제 이름은 {name} 입니다. <br />
